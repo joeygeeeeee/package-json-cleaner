d646222ea79631f8182d47119b8a3fb8d5c0e17f
Joseph Gordon <me@josephgordon.co.uk>
2016-02-24 13:22:19 +0000

    correct function name

--- src/lib/get-dubious-packages.js
+++ src/lib/get-dubious-packages.js
@@ -4,7 +4,7 @@ export default function getDubiousPackages(packageUsages, rules) {
 
     var listOfPredicates = R.map(rule => R.compose(R.length, R.match(rule)), rules);
     var passesAnyRules = R.anyPass(listOfPredicates);
-    var anyPassAllRules = R.any(passesAnyRules);
-    var noneDubiousUsage = R.compose(anyPassAllRules, R.map(pkg => R.prop('usage', pkg)));
+    var anyPassAnyRules = R.any(passesAnyRules);
+    var noneDubiousUsage = R.compose(anyPassAnyRules, R.map(pkg => R.prop('usage', pkg)));
     return R.reject(noneDubiousUsage, packageUsages);
 }
